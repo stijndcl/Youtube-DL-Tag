54d2a006b3ecce7ff421a851fa528bdd9f68e0c7
Stijn De Clercq <stijn.declercq@ugent.be>
2020-07-25 18:10:48 +0200

    reworked progress bar

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import sys
+import time
+
 import requests
 import json
 import eyed3
@@ -20,11 +22,13 @@ class Logger(object):
 
 def hook(d):
     if d["status"] == "downloading":
-        progressBar.progress(int(d["_percent_str"].split(".")[0]))
-        # print("\r Downloaded {}/{} bytes ({}) at {}".format(d["downloaded_bytes"], d["total_bytes"],
-        #       d["_percent_str"].strip(), d["_speed_str"]))
+        # progressBar.progress(int(d["_percent_str"].split(".")[0]))
+        print("Downloaded {:,}/{:,} bytes ({}) at {} - ETA: {} seconds {}".format(
+            int(d["downloaded_bytes"]), int(d["total_bytes"]), d["_percent_str"].strip(),
+            d["_speed_str"].strip(), d["eta"], progressBar.createProgressBar(int(d["_percent_str"].split(".")[0]))
+            ),
+              end="\r")
     if d['status'] == 'finished':
-        progressBar.endProgress()
         print('Converting to {}...'.format(d["filename"][:-4] + "mp3"))
 
 
@@ -41,8 +45,7 @@ def download(link: str):
         'progress_hooks': [hook],
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        progressBar.startProgress("Progress")
         ydl.download([link])
 
 
-download(sys.argv[1])
+# download(sys.argv[1])
